6c1746a978e7b596d05fa9168726da6eef0d6c22
Wechuli <wechulipaul@yahoo.com>
2023-03-08 14:33:34 +0300

    Update configure.js

--- bin/configure.js
+++ bin/configure.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
 const { execSync } = require("child_process");
+console.log(process.version);
 execSync('echo "Yarn version: `yarn --version`"', { stdio: "inherit" });
 execSync(`yarn --cwd "${__dirname}/../" install --prod  --non-interactive --no-progress`, { stdio: "inherit" });
 execSync(`yarn --cwd "${__dirname}/../" list --depth=0 --prod --non-interactive --no-progress`, { stdio: "inherit" });
